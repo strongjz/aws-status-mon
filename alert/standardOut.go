5170f1245ca805cb0c3306645cce66383fd17bea
James Strong <strong.james.e@gmail.com>
2017-09-19 15:22:33 -0400

    added shell files for alerting

---
+++ alert/standardOut.go
@@ -0,0 +1,10 @@
+package alert
+
+import "log"
+
+//StandardOut - Logging the alert to Standard out
+func StandardOut(service string, region string, whatsup string) {
+
+	log.Printf("[ALERT] %s in %s %s\n", service, region, whatsup)
+
+}
